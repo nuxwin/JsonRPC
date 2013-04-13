c69b3143371b2128f8d004ab8b7b09c948451a62
Frederic Guillot <contact@fredericguillot.com>
2013-04-13 11:40:53 -0400

    Add unit test and batch implementation

--- src/JsonRPC/Server.php
+++ src/JsonRPC/Server.php
@@ -4,11 +4,11 @@ namespace JsonRPC;
 
 class Server
 {
-    private $payload = array();
-    private $procedures = array();
+    private $payload;
+    static private $procedures = array();
 
 
-    public function __construct(array $payload = array())
+    public function __construct($payload = '')
     {
         $this->payload = $payload;
     }
@@ -25,20 +25,36 @@ class Server
 
     public function register($name, \Closure $callback)
     {
-        $this->procedures[$name] = $callback;
+        self::$procedures[$name] = $callback;
     }
 
 
-    public function getResponse(array $data)
+    public function unregister($name)
     {
-        $response = array(
-            'jsonrpc' => '2.0'
-        );
+        if (isset(self::$procedures[$name])) {
 
-        if (isset($this->payload['id'])) {
-
-            $response['id'] = $this->payload['id'];
+            unset(self::$procedures[$name]);
         }
+    }
+
+
+    public function unregisterAll()
+    {
+        self::$procedures = array();
+    }
+
+
+    public function getResponse(array $data, array $payload = array())
+    {
+        if (! array_key_exists('id', $payload)) {
+
+            return '';
+        }
+
+        $response = array(
+            'jsonrpc' => '2.0',
+            'id' => $payload['id']
+        );
 
         $response = array_merge($response, $data);
 
@@ -78,49 +94,87 @@ class Server
 
     public function execute()
     {
-        // Check JSON format
+        // Parse payload
         if (empty($this->payload)) {
 
-            $this->payload = json_decode(file_get_contents('php://input'), true);
+            $this->payload = file_get_contents('php://input');
+        }
 
-            if (! $this->payload) {
+        if (is_string($this->payload)) {
 
-                return $this->getResponse(array(
-                    'error' => array(
-                        'code' => -32700,
-                        'message' => 'Parse error.'
-                    )
-                ));
+            $this->payload = json_decode($this->payload, true);
+        }
+
+        // Check JSON format
+        if (! is_array($this->payload)) {
+
+            return $this->getResponse(array(
+                'error' => array(
+                    'code' => -32700,
+                    'message' => 'Parse error'
+                )),
+                array('id' => null)
+            );
+        }
+
+        // Handle batch request
+        if (array_keys($this->payload) === range(0, count($this->payload) - 1)) {
+
+            $responses = array();
+
+            foreach ($this->payload as $payload) {
+
+                if (! is_array($payload)) {
+
+                    $responses[] = $this->getResponse(array(
+                        'error' => array(
+                            'code' => -32600,
+                            'message' => 'Invalid Request'
+                        )),
+                        array('id' => null)
+                    );
+                }
+                else {
+
+                    $server = new Server($payload);
+                    $response = $server->execute();
+
+                    if ($response) $responses[] = $response;
+                }
             }
+
+            return empty($responses) ? '' : '['.implode(',', $responses).']';
         }
 
         // Check JSON-RPC format
         if (! isset($this->payload['jsonrpc']) ||
             ! isset($this->payload['method']) ||
             ! is_string($this->payload['method']) ||
-            $this->payload['jsonrpc'] != '2.0' ||
+            $this->payload['jsonrpc'] !== '2.0' ||
             (isset($this->payload['params']) && ! is_array($this->payload['params']))) {
 
             return $this->getResponse(array(
                 'error' => array(
                     'code' => -32600,
-                    'message' => 'Invalid Request.'
-                )
-            ));
+                    'message' => 'Invalid Request'
+                )),
+                array('id' => null)
+            );
         }
 
         // Procedure not found
-        if (! isset($this->procedures[$this->payload['method']])) {
+        if (! isset(self::$procedures[$this->payload['method']])) {
 
             return $this->getResponse(array(
                 'error' => array(
                     'code' => -32601,
-                    'message' => 'Procedure not found.'
-                )
-            ));
+                    'message' => 'Method not found'
+                )),
+                $this->payload
+            );
         }
 
-        $callback = $this->procedures[$this->payload['method']];
+        $callback = self::$procedures[$this->payload['method']];
         $params = array();
 
         $reflection = new \ReflectionFunction($callback);
@@ -134,14 +188,15 @@ class Server
                 return $this->getResponse(array(
                     'error' => array(
                         'code' => -32602,
-                        'message' => 'Invalid params.'
-                    )
-                ));
+                        'message' => 'Invalid params'
+                    )),
+                    $this->payload
+                );
             }
         }
 
         $result = $reflection->invokeArgs($params);
 
-        return $this->getResponse(array('result' => $result));
+        return $this->getResponse(array('result' => $result), $this->payload);
     }
 }
